729bb055629c480b2a75656aa78c44f03841b6ca
ihor <ihors@wizardsdev.com>
2019-11-11 09:13:01 +0200

    refactoring

--- uaihorshulha/src/main/java/ua/ihorshulha/Main.java
+++ uaihorshulha/src/main/java/ua/ihorshulha/Main.java
@@ -1,6 +1,5 @@
-package ua.ihorshulha.HW_01_Guess_the_number;
+package ua.ihorshulha;
 
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         int input = 0;
+        int count;
         Scanner scanner = new Scanner(System.in);
         Random generator = new Random();
 
@@ -15,24 +15,28 @@ public class Main {
 
         System.out.println("My app is thinking of a number between 0 and 1000.");
         System.out.println("Guess it! Input integer, please...");
+        count = 1;
         do {
             String str = scanner.next();
             if (isCorrectInteger(str)) {
                 input = Integer.parseInt(str);
 
                 if (input < 0 || input > 1000) {
-                    System.out.println("Number between 0 and 1000...");
+                    System.out.println("Number between 0 and 1000... Try again");
+                    count++;
                 }
                 if (input > random) {
-                    System.out.println("Less!");
+                    System.out.println("Too high! Try again");
+                    count++;
                 } else if (input < random) {
-                    System.out.println("More!");
+                    System.out.println("Too low! Try again");
+                    count++;
                 }
             }
         }
         while (input != random);
 
-        System.out.print(random + "is the correct answer and it took you");
+        System.out.print(random + " - You guessed for " + count + " times");
     }
 
 
@@ -40,9 +44,10 @@ public class Main {
         try {
             Integer.parseInt(str);
         } catch (NumberFormatException e) {
-            System.out.println("Enter only number");
+            System.out.println("Enter only number. Try again.");
             return false;
         }
         return true;
     }
 }
+
