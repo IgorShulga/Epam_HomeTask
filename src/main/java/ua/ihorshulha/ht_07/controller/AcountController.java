023d151c49ea19afbd60cb23cb79f64346d7e918
IgorShulga <igor.shulga8@gmail.com>
2019-12-15 18:58:43 +0200

    add methods controllers

--- src/main/java/ua/ihorshulha/ht_07/controller/AcountController.java
+++ src/main/java/ua/ihorshulha/ht_07/controller/AcountController.java
@@ -1,5 +1,22 @@
 package ua.ihorshulha.ht_07.controller;
 
+import java.io.BufferedReader;
+
 public class AcountController {
 
+    public void getAllAccounts() {
+    }
+
+    public void getAccountById(BufferedReader inputKeyboard) {
+    }
+
+    public void addAccount(BufferedReader inputKeyboard) {
+    }
+
+    public void updateAccountById(BufferedReader inputKeyboard) {
+    }
+
+    public void removeAccountById(BufferedReader inputKeyboard) {
+
+    }
 }
--- src/main/java/ua/ihorshulha/ht_07/controller/DeveloperController.java
+++ src/main/java/ua/ihorshulha/ht_07/controller/DeveloperController.java
@@ -1,5 +1,26 @@
 package ua.ihorshulha.ht_07.controller;
 
+import java.io.BufferedReader;
+
 public class DeveloperController {
 
+    public void getAllDevelopers() {
+
+    }
+
+    public void getDeveloperById(BufferedReader inputKeyboard) {
+
+    }
+
+    public void addDeveloper(BufferedReader inputKeyboard) {
+
+    }
+
+    public void updateDeveloperById(BufferedReader inputKeyboard) {
+
+    }
+
+    public void removeDeveloperById(BufferedReader inputKeyboard) {
+
+    }
 }
